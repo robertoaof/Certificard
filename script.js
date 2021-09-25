6edc962ab54b339db8d6f56ad2788856f769f9a1
robertoaof <91355121+robertoaof@users.noreply.github.com>
2021-09-25 01:49:53 -0300

    adicionei os arquivos do certificard

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudarTema() {
+  document.body.classList.toggle("dark");
+}
+
+/*Desafios desta aula! (9)
+-- Terminar todo o design que está no figma (disponível abaixo)
+-- Adicionar suas redes sociais de forma que quem clique consiga abrí-las*/
